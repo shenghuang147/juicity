748a9b27897735ef581c3a9d5e25e1f1521c4271
ShengHuang <shenghuang147@gmail.com>
2024-05-13 00:01:00 +0800

    chore: disable gso on client by default

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,10 @@ package main
 import "os"
 
 func main() {
+        if _, ok := os.LookupEnv("QUIC_GO_DISABLE_GSO"); !ok {
+                os.Setenv("QUIC_GO_DISABLE_GSO", "1")
+        }
+
 	if err := Execute(); err != nil {
 		os.Exit(1)
 	}
